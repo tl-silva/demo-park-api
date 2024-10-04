ab45436c9e3e7547197b583d38631dd5739135e5
Thalita Silva <thalitassilva2@gmail.com>
2024-10-04 10:34:35 -0300

    Entity: User

---
+++ src/main/java/com/mballem/demoparkapi/entity/User.java
@@ -0,0 +1,88 @@
+package com.mballem.demoparkapi.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "username", nullable = false, unique = true, length = 100)
+	private String username;
+	
+	@Column(name = "password", nullable = false, length = 200)
+	private String password;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "role", nullable = false, length = 25)
+	private Role role;
+	
+	@Column(name = "created_date")
+	private LocalDateTime createdDate;
+	
+	@Column(name = "last_modified_date")
+	private LocalDateTime lastModifiedDate; 
+	
+	@Column(name = "created_by")
+	private String createdBy;
+	
+	@Column(name = "last_modified_by")
+	private String lastModifiedBy;
+	
+	
+	public enum Role {
+		ROLE_ADMIN, ROLE_CLIENT
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User{"
+				+ "id=" + id +
+				"}";
+	}
+	
+}
