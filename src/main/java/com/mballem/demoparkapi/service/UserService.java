51a5706c37ebaaffebff573d3af03ebeb8155739
Thalita Silva <thalitassilva2@gmail.com>
2024-10-04 14:03:07 -0300

    User create method

--- src/main/java/com/mballem/demoparkapi/service/UserService.java
+++ src/main/java/com/mballem/demoparkapi/service/UserService.java
@@ -1,7 +1,9 @@
 package com.mballem.demoparkapi.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.mballem.demoparkapi.entity.User;
 import com.mballem.demoparkapi.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,5 +13,10 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 	
 	private final UserRepository userRepository;
+
+	@Transactional
+	public User save(User user) {
+		return userRepository.save(user);
+	}
 
 }
--- src/main/java/com/mballem/demoparkapi/web/controller/UserController.java
+++ src/main/java/com/mballem/demoparkapi/web/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.mballem.demoparkapi.web.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mballem.demoparkapi.entity.User;
 import com.mballem.demoparkapi.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -13,5 +18,12 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 	
 	private final UserService userService;
+	
+	@PostMapping()
+	public ResponseEntity<User> create(@RequestBody User user){
+		User newUser = userService.save(user);
+		return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+	}
+	
 
 }
