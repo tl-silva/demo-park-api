116e32a5b85ef4dffcee3a267c5ff42695af269e
Thalita Silva <thalitassilva2@gmail.com>
2024-10-13 23:41:55 -0300

    Test: Conflict Exception (409)

--- src/test/java/com/mballem/demoparkapi/UserIT.java
+++ src/test/java/com/mballem/demoparkapi/UserIT.java
@@ -123,4 +123,19 @@ public class UserIT {
 
 	}
 
+	@Test
+	public void createUser_WithRepeatedUsername_ReturnUserCreatedWithStatus409() {
+		ErrorMessage responseBody = testClient
+				.post()
+				.uri("/api/v1/users")
+				.contentType(MediaType.APPLICATION_JSON)
+				.bodyValue(new UserCreateDto("ebechara@email.com", "123456"))
+				.exchange()
+				.expectStatus().isEqualTo(409)
+				.expectBody(ErrorMessage.class)
+				.returnResult().getResponseBody();
+		
+		org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+		org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(409);
+	}
 }
