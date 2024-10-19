95730b22d3b79ecdb7beb4a573e8ee74a9f9e85c
Thalita Silva <thalitassilva2@gmail.com>
2024-10-19 19:35:00 -0300

    Test: Implementing authentication

---
+++ src/test/java/com/mballem/demoparkapi/JwtAuthentication.java
@@ -0,0 +1,25 @@
+package com.mballem.demoparkapi;
+
+import java.util.function.Consumer;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.mballem.demoparkapi.jwt.JwtToken;
+import com.mballem.demoparkapi.web.dto.UserLoginDto;
+
+public class JwtAuthentication {
+	
+	public static Consumer<HttpHeaders> getHeaderAuthorization(WebTestClient client, String username, String password) {
+		String token = client
+				.post()
+				.uri("/api/v1/auth")
+				.bodyValue(new UserLoginDto(username, password))
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(JwtToken.class)
+				.returnResult().getResponseBody().getToken();
+		return headers -> headers.add(HttpHeaders.AUTHORIZATION, "Bearer" + token);
+	}
+
+}
