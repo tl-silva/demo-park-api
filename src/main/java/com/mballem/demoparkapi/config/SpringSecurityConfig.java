cf825aa66eed31da18e8af1a28d924af09587b0f
Thalita Silva <thalitassilva2@gmail.com>
2024-10-18 21:48:15 -0300

    Handling errors: 401 (Unauthorized) and 403 (Forbidden)

--- src/main/java/com/mballem/demoparkapi/config/SpringSecurityConfig.java
+++ src/main/java/com/mballem/demoparkapi/config/SpringSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.mballem.demoparkapi.jwt.JwtAuthenticationEntryPoint;
 import com.mballem.demoparkapi.jwt.JwtAuthorizationFilter;
 
 @EnableMethodSecurity
@@ -35,6 +36,8 @@ public class SpringSecurityConfig {
 						session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 				).addFilterBefore(
 						jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class
+				).exceptionHandling(ex -> ex
+						.authenticationEntryPoint(new JwtAuthenticationEntryPoint())
 				).build();
 	}
 	
--- src/main/java/com/mballem/demoparkapi/web/exception/ApiExceptionHandler.java
+++ src/main/java/com/mballem/demoparkapi/web/exception/ApiExceptionHandler.java
@@ -3,6 +3,7 @@ package com.mballem.demoparkapi.web.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,6 +20,15 @@ import lombok.extern.slf4j.Slf4j;
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
+	@ExceptionHandler(AccessDeniedException.class)
+	public ResponseEntity<ErrorMessage> accessDeniedException(AccessDeniedException ex, HttpServletRequest request){
+		log.error("Api Error - ", ex);
+		return ResponseEntity
+				.status(HttpStatus.FORBIDDEN)
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(new ErrorMessage(request, HttpStatus.FORBIDDEN, ex.getMessage()));
+	}
+	
 	@ExceptionHandler(PasswordInvalidException.class)
 	public ResponseEntity<ErrorMessage> passwordInvalidException(RuntimeException ex, HttpServletRequest request){
 		log.error("Api Error - ", ex);
