45b4d5808a0efdbc69c6661ca0ccb7ca6a331ee9
Thalita Silva <thalitassilva2@gmail.com>
2024-10-18 21:57:49 -0300

    UpdatePassword: Acess Permission

--- src/main/java/com/mballem/demoparkapi/web/controller/UserController.java
+++ src/main/java/com/mballem/demoparkapi/web/controller/UserController.java
@@ -83,6 +83,7 @@ public class UserController {
 					
 			})
 	@PatchMapping("/{id}")
+	@PreAuthorize("hasAnyRole('ADMIN', 'CLIENT') AND (#id == authentication.principal.id)")
 	public ResponseEntity<Void> updatePassword(@PathVariable Long id, @Valid @RequestBody UserPasswordDto userPasswordDto){
 		User newUser = userService.editPassword(id, userPasswordDto.getCurrentPassword(), userPasswordDto.getNewPassword(), userPasswordDto.getConfirmPassword());
 		return ResponseEntity.noContent().build();
