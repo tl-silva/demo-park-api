37c6d4cb7625fead0e5381baeb03a05776aa58bf
Thalita Silva <thalitassilva2@gmail.com>
2024-10-22 22:53:29 -0300

    Test: Create Client with invalid data and no permission | 409 | 422 | 403

--- src/test/java/com/mballem/demoparkapi/ClientIT.java
+++ src/test/java/com/mballem/demoparkapi/ClientIT.java
@@ -9,6 +9,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 import com.mballem.demoparkapi.web.dto.ClientCreateDto;
 import com.mballem.demoparkapi.web.dto.ClientResponseDto;
+import com.mballem.demoparkapi.web.exception.ErrorMessage;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = "/sql/clients/clients-insert.sql", executionPhase =  Sql.ExecutionPhase.BEFORE_TEST_METHOD)
@@ -38,6 +39,88 @@ public class ClientIT {
 		org.assertj.core.api.Assertions.assertThat(responseBody.getCpf()).isEqualTo("24205734042");
 		
 	}
+	
+	@Test
+	public void createClient_WithAlreadyRegisteredCpf_ReturnErrorMessageWithStatus409() {
+		ErrorMessage responseBody = testClient
+				.post()
+				.uri("/api/v1/clients")
+				.contentType(MediaType.APPLICATION_JSON)
+				.headers(JwtAuthentication.getHeaderAuthorization(testClient, "cveloso@email.com", "123456"))
+				.bodyValue(new ClientCreateDto("Caetano Veloso", "19913027039"))
+				.exchange()
+				.expectStatus().isEqualTo(409)
+				.expectBody(ErrorMessage.class)
+				.returnResult().getResponseBody();
 		
+		org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+		org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(409);
+		
+	}
+	
+	@Test
+	public void createClient_WithInvalidData_ReturnErrorMessageWithStatus422() {
+		ErrorMessage responseBody = testClient
+				.post()
+				.uri("/api/v1/clients")
+				.contentType(MediaType.APPLICATION_JSON)
+				.headers(JwtAuthentication.getHeaderAuthorization(testClient, "cveloso@email.com", "123456"))
+				.bodyValue(new ClientCreateDto("", ""))
+				.exchange()
+				.expectStatus().isEqualTo(422)
+				.expectBody(ErrorMessage.class)
+				.returnResult().getResponseBody();
+		
+		org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+		org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+		
+		responseBody = testClient
+				.post()
+				.uri("/api/v1/clients")
+				.contentType(MediaType.APPLICATION_JSON)
+				.headers(JwtAuthentication.getHeaderAuthorization(testClient, "cveloso@email.com", "123456"))
+				.bodyValue(new ClientCreateDto("Nand", "00000000000"))
+				.exchange()
+				.expectStatus().isEqualTo(422)
+				.expectBody(ErrorMessage.class)
+				.returnResult().getResponseBody();
+		
+		org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+		org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+		
+		responseBody = testClient
+				.post()
+				.uri("/api/v1/clients")
+				.contentType(MediaType.APPLICATION_JSON)
+				.headers(JwtAuthentication.getHeaderAuthorization(testClient, "cveloso@email.com", "123456"))
+				.bodyValue(new ClientCreateDto("Nand", "242.057.340-42"))
+				.exchange()
+				.expectStatus().isEqualTo(422)
+				.expectBody(ErrorMessage.class)
+				.returnResult().getResponseBody();
+		
+		org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+		org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+		
+	}
+
+	@Test
+	public void createClient_WithUserNotAllowed_ReturnErrorMessageWithStatus403() {
+		ErrorMessage responseBody = testClient
+				.post()
+				.uri("/api/v1/clients")
+				.contentType(MediaType.APPLICATION_JSON)
+				.headers(JwtAuthentication.getHeaderAuthorization(testClient, "celler@email.com", "123456"))
+				.bodyValue(new ClientCreateDto("Caetano Veloso", "24205734042"))
+				.exchange()
+				.expectStatus().isForbidden()
+				.expectBody(ErrorMessage.class)
+				.returnResult().getResponseBody();
+		
+		org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+		org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(403);
+		
+	}
+	
 
 }
