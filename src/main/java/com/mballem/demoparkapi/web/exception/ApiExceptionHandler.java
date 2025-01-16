785b2c083ecb376d9b92d09c742ce1d6e6bce282
Thalita Silva <thalitassilva2@gmail.com>
2025-01-16 15:40:03 -0300

    JasperService Class to generate the Report via API

--- src/main/java/com/mballem/demoparkapi/web/exception/ApiExceptionHandler.java
+++ src/main/java/com/mballem/demoparkapi/web/exception/ApiExceptionHandler.java
@@ -68,5 +68,16 @@ public class ApiExceptionHandler {
 				.contentType(MediaType.APPLICATION_JSON)
 				.body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Invalid field(s)", result));
 	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorMessage> InternalServerErrorException(Exception ex, HttpServletRequest request) {
+		ErrorMessage error = new ErrorMessage(
+				request, HttpStatus.INTERNAL_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+		log.error("Internal Server Error {} {} ", error, ex.getMessage());
+		return ResponseEntity
+				.status(HttpStatus.INTERNAL_SERVER_ERROR)
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(error);
+	}
 
 }
